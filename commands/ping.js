47e96a726e1558d85f750e8db587718d9a5240a1
asep <bikingimil@gmal.com>
2026-04-22 20:36:26 +0800

    Refactor 'ping' command to calculate latency and add new 'hi' command for greeting

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,9 @@
 module.exports = {
     name: 'ping',
-    execute(message) {
-        message.reply('Heyy Antek Antek Asing');
+    async execute(message) {
+        // Calculate ping (latency)
+        const sent = await message.reply('Menghitung ping...');
+        const ping = sent.createdTimestamp - message.createdTimestamp;
+        sent.edit(`Ping Saudara Bangsa dan setanah Air: ${ping}ms`);
     }
 };
